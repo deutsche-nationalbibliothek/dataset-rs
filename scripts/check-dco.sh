3a015388036bab578b93ad339dc28c4f45ac4fef
Nico Wagner <n.wagner@dnb.de>
2024-03-21 13:15:38 +0100

    DCO

---
+++ scripts/check-dco.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+commits=$(git log --grep 'Signed-off-by: ' --invert-grep --format='%H')
+
+if [[ ! -z $commits ]]; then
+    for commit in $commits; do
+        >&2 echo "Missing 'Signed-off-by' tag for commit '${commit:0:24}'"
+    done
+
+    exit 1
+fi
+
