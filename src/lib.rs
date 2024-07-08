108eba391b1d884a88eced43302bc2774c13ffce
Nico Wagner <n.wagner@dnb.de>
2024-07-08 18:23:38 +0200

    Add project description (#4)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! # Dataset
+//!
+//! This project provides tools to create an manage datasets. The
+//! documents are expected to be in plain text format, whereby most of
+//! the underlying functions work on byte strings (slices), which are
+//! are only _conventionally_ UTF-8.
+//!
+//! These tools are developments to support the research work in the
+//! Automated Indexing System project of the [German National Library].
+//! **The tools are not recommended for productive use; no support is
+//! provided.**
+//!
+//! This project provides the following tools:
+//!
+//! ## Datapod
+//!
+//! The `datapod` tool creates and manages a _datapod_ that can later be
+//! used in a dataset as an data source. It offers an index of the
+//! managed documents, which contains various metrics for determining
+//! the quality of a document.
+//!
+//! ## Contributing
+//!
+//! All contributors are required to "sign-off" their commits (using
+//! `git commit -s`) to indicate that they have agreed to the [Developer
+//! Certificate of Origin][DCO].
+//!
+//! ## License
+//!
+//! This project is licensed under the terms of the [EUPL v1.2].
+//!
+//! [German National Library]: https://www.dnb.de
+//! [DCO]: https://developercertificate.org/
+//! [EUPL v1.2]: https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
