f54b5f15857d22b27832e9a87465e906518473e7
Nico Wagner <n.wagner@dnb.de>
2024-07-09 09:43:44 +0200

    datapod: initialize global thread pool
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/datapod/src/cli.rs
+++ crates/datapod/src/cli.rs
@@ -7,6 +7,17 @@ use crate::commands::version::Version;
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 pub(crate) struct Args {
+    /// Number of threads to use. If this options isn't set or a value
+    /// of "0" is chosen, the maximum number of available threads
+    /// is used.
+    #[clap(
+        short = 'j',
+        long,
+        env = "DATASET_NUM_JOBS",
+        hide_env_values = true
+    )]
+    pub(crate) num_jobs: Option<usize>,
+
     #[command(subcommand)]
     pub(crate) cmd: Command,
 }
