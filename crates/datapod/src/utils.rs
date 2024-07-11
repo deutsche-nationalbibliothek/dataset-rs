b6a971e7dd19ca5232d4b20a8ff8cdf1e317f3f8
Nico Wagner <n.wagner@dnb.de>
2024-07-11 15:41:56 +0200

    datapod: add `status` command
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/datapod/src/utils.rs
+++ crates/datapod/src/utils.rs
@@ -1,7 +1,11 @@
 use std::path::Path;
 
 #[inline]
-pub(crate) fn relpath<P: AsRef<Path>>(path: P, prefix: P) -> String {
+pub(crate) fn relpath<P1, P2>(path: P1, prefix: P2) -> String
+where
+    P1: AsRef<Path>,
+    P2: AsRef<Path>,
+{
     path.as_ref()
         .strip_prefix(prefix)
         .expect("valid prefix")
