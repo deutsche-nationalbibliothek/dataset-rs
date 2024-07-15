9a73cf0a9933340a8083f994a3a74319d98a5a95
Nico Wagner <n.wagner@dnb.de>
2024-07-15 11:32:27 +0000

    datashed: add `--bump` option (version)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/datashed/src/commands/version.rs
+++ crates/datashed/src/commands/version.rs
@@ -1,6 +1,14 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
+use semver::Version as SemVer;
 
 use crate::prelude::*;
+
+#[derive(Debug, Clone, ValueEnum)]
+enum Bump {
+    Major,
+    Minor,
+    Patch,
+}
 
 /// Get or set the version of the datashed.
 #[derive(Debug, Parser)]
@@ -9,12 +17,27 @@ pub(crate) struct Version {
     #[arg(short, long)]
     force: bool,
 
+    /// Run verbosely. Print additional progress information to the
+    /// standard error stream. This option conflicts with the
+    /// `--quiet` option.
+    #[arg(short, long, conflicts_with = "quiet")]
+    verbose: bool,
+
+    /// Operate quietly; do not show progress. This option conflicts
+    /// with the `--verbose` option.
+    #[arg(short, long, conflicts_with = "verbose")]
+    quiet: bool,
+
+    #[arg(short, long, conflicts_with = "version")]
+    bump: Option<Bump>,
+
     /// The new version of the datashed. Unless the `--force`/`-f`
     /// option is set, the new version must be greater than the
     /// current version. A datashed version consists of three
     /// separated integers, which must conform to the semantic
     /// versioning standard; invalid version strings are rejected.
-    version: Option<semver::Version>,
+    #[arg(conflicts_with = "bump")]
+    version: Option<SemVer>,
 }
 
 impl Version {
@@ -26,6 +49,23 @@ impl Version {
             if !self.force && version <= config.metadata.version {
                 let current = config.metadata.version.to_string();
                 bail!("{version} must be greater than {current}");
+            }
+
+            config.metadata.version = version;
+            config.save()?;
+        } else if let Some(bump) = self.bump {
+            let major = config.metadata.version.major;
+            let minor = config.metadata.version.minor;
+            let patch = config.metadata.version.patch;
+
+            let version = match bump {
+                Bump::Patch => SemVer::new(major, minor, patch + 1),
+                Bump::Minor => SemVer::new(major, minor + 1, 0),
+                Bump::Major => SemVer::new(major + 1, 0, 0),
+            };
+
+            if self.verbose {
+                println!("bumped version to {version}");
             }
 
             config.metadata.version = version;
