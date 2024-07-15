32245303c89424070425f8259583ceb1ef04f16a
Nico Wagner <n.wagner@dnb.de>
2024-07-15 13:07:15 +0200

    datashed: cleanup `version` command (#32)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/datashed/src/commands/version.rs
+++ crates/datashed/src/commands/version.rs
@@ -2,15 +2,18 @@ use clap::Parser;
 
 use crate::prelude::*;
 
-/// Get or set the version of the data pod.
+/// Get or set the version of the datashed.
 #[derive(Debug, Parser)]
 pub(crate) struct Version {
     /// Whether to overwrite the current version or not.
     #[arg(short, long)]
     force: bool,
 
-    /// The new version of the data pod. Unless the `--force` option is
-    /// set, the new version must be greater than the current version.
+    /// The new version of the datashed. Unless the `--force`/`-f`
+    /// option is set, the new version must be greater than the
+    /// current version. A datashed version consists of three
+    /// separated integers, which must conform to the semantic
+    /// versioning standard; invalid version strings are rejected.
     version: Option<semver::Version>,
 }
 
@@ -21,11 +24,8 @@ impl Version {
 
         if let Some(version) = self.version {
             if !self.force && version <= config.metadata.version {
-                bail!(
-                    "{} must be greater than {}",
-                    version,
-                    config.metadata.version
-                );
+                let current = config.metadata.version.to_string();
+                bail!("{version} must be greater than {current}");
             }
 
             config.metadata.version = version;
