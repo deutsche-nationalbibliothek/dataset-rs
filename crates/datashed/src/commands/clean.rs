a24ebfb0f4bb50a987575a61c572b215e344d8d5
Nico Wagner <n.wagner@dnb.de>
2024-07-16 09:22:16 +0000

    latest
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/datashed/src/commands/clean.rs
+++ crates/datashed/src/commands/clean.rs
@@ -1,15 +1,20 @@
 use std::collections::BTreeSet;
-use std::fs::remove_file;
+use std::fs::{remove_file, File};
 
 use clap::Parser;
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Confirm;
-use glob::{glob_with, MatchOptions};
+use glob::glob_with;
+use indicatif::ProgressIterator;
+use polars::prelude::*;
 
-// use polars::prelude::*;
 use crate::datashed::Datashed;
 use crate::error::{DatashedError, DatashedResult};
+use crate::progress::ProgressBarBuilder;
 use crate::utils::relpath;
+
+const PBAR_COLLECT: &str = "Collecting documents: {human_pos} | \
+        elapsed: {elapsed_precise}{msg}";
 
 #[derive(Debug, Default, Parser)]
 pub(crate) struct Clean {
@@ -23,6 +28,10 @@ pub(crate) struct Clean {
     /// with the `--verbose` option.
     #[arg(short, long, conflicts_with = "verbose")]
     quiet: bool,
+
+    /// Whether to confirm delete operations or not.
+    #[arg(short, long)]
+    force: bool,
 }
 
 impl Clean {
@@ -32,14 +41,17 @@ impl Clean {
         let base_dir = datashed.base_dir();
 
         let pattern = format!("{}/**/*.txt", data_dir.display());
-        let options = MatchOptions::default();
+        let pbar =
+            ProgressBarBuilder::new(PBAR_COLLECT, self.quiet).build();
 
         let mut missing: Vec<_> = vec![];
-        let mut untracked: BTreeSet<_> = glob_with(&pattern, options)
-            .map_err(|e| DatashedError::Other(e.to_string()))?
-            .filter_map(Result::ok)
-            .map(|path| relpath(path, base_dir))
-            .collect();
+        let mut untracked: BTreeSet<_> =
+            glob_with(&pattern, Default::default())
+                .map_err(|e| DatashedError::Other(e.to_string()))?
+                .progress_with(pbar)
+                .filter_map(Result::ok)
+                .map(|path| relpath(path, base_dir))
+                .collect();
 
         let index = datashed.index()?;
         let path = index.column("path")?.str()?;
@@ -47,16 +59,14 @@ impl Clean {
         for idx in 0..index.height() {
             let index_path = path.get(idx).unwrap();
 
-            if untracked.remove(index_path) {
-                continue;
-            } else {
+            if !untracked.remove(index_path) {
                 missing.push(index_path);
             }
         }
 
         if !untracked.is_empty() {
-            let confirm =
-                Confirm::with_theme(&ColorfulTheme::default())
+            let confirm = self.force
+                || Confirm::with_theme(&ColorfulTheme::default())
                     .with_prompt(format!(
                         "Delete {} untracked document(s))?",
                         untracked.len()
@@ -75,17 +85,29 @@ impl Clean {
         }
 
         if !missing.is_empty() {
-            println!("{}", index);
+            let confirm = self.force
+                || Confirm::with_theme(&ColorfulTheme::default())
+                    .with_prompt(format!(
+                        "Delete {} missing index entries)?",
+                        missing.len()
+                    ))
+                    .default(true)
+                    .show_default(true)
+                    .interact()
+                    .unwrap();
 
-            // let missing = Series::from_iter(missing);
+            if confirm {
+                let missing = Series::from_iter(missing);
+                let mut df = index
+                    .lazy()
+                    .filter(col("path").is_in(lit(missing)).not())
+                    .collect()?;
 
-            // println!(
-            //     "{:?}",
-            //     index
-            //         .lazy()
-            //         .filter(col("path").is_in(lit(missing)))
-            //         .collect()
-            // );
+                let path = base_dir.join(Datashed::INDEX);
+                let mut writer = IpcWriter::new(File::create(path)?)
+                    .with_compression(Some(IpcCompression::ZSTD));
+                writer.finish(&mut df)?;
+            }
         }
 
         Ok(())
