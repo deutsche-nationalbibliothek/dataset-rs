34c3f0905346b1ae04fdfda59252a18bbc813dff
Nico Wagner <n.wagner@dnb.de>
2024-08-12 15:31:10 +0200

    dataset: initialize global thread pool (#92)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/dataset/src/cli.rs
+++ crates/dataset/src/cli.rs
@@ -2,4 +2,15 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None, max_term_width = 72)]
-pub(crate) struct Args {}
+pub(crate) struct Args {
+    /// Number of threads to use. If this options isn't set or a value
+    /// of "0" is chosen, the maximum number of available threads
+    /// is used.
+    #[clap(
+        short = 'j',
+        long,
+        env = "DATASET_NUM_JOBS",
+        hide_env_values = true
+    )]
+    pub(crate) num_jobs: Option<usize>,
+}
--- crates/dataset/src/main.rs
+++ crates/dataset/src/main.rs
@@ -3,9 +3,18 @@ use std::process;
 use clap::Parser;
 use cli::Args;
 use error::DatashedResult;
+use rayon::ThreadPoolBuilder;
 
 mod cli;
 mod error;
+
+fn num_threads(args: &Args) -> usize {
+    if let Some(num_threads) = args.num_jobs {
+        return num_threads;
+    }
+
+    0
+}
 
 fn run(_args: Args) -> DatashedResult<()> {
     Ok(())
@@ -14,11 +23,12 @@ fn run(_args: Args) -> DatashedResult<()> {
 fn main() {
     let args = Args::parse();
 
-    match run(args) {
-        Ok(()) => process::exit(0),
-        Err(e) => {
-            eprintln!("error: {e:#}");
-            process::exit(1);
-        }
+    ThreadPoolBuilder::new()
+        .num_threads(num_threads(&args))
+        .build_global()
+        .unwrap();
+
+    if run(args).is_ok() {
+        process::exit(0);
     }
 }
