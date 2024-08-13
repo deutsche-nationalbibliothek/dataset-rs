12062b57768b4a7bceb4998d7e44ab587fa691aa
Nico Wagner <n.wagner@dnb.de>
2024-08-13 12:19:45 +0200

    dataset: add `version` command (#98)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/dataset/src/commands/mod.rs
+++ crates/dataset/src/commands/mod.rs
@@ -1,7 +1,9 @@
 pub(crate) use completions::Completions;
 pub(crate) use config::Config;
 pub(crate) use init::Init;
+pub(crate) use version::Version;
 
 mod completions;
 mod config;
 mod init;
+mod version;
