0540b764b81abf98203e0aa5c208fe9ba008fae5
Nico Wagner <n.wagner@dnb.de>
2024-08-16 07:53:32 +0000

    datashed: move prelude module ino file
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

---
+++ crates/datashed/src/prelude.rs
@@ -0,0 +1,5 @@
+pub(crate) use crate::config::{Config, Runtime};
+pub(crate) use crate::datashed::Datashed;
+pub(crate) use crate::document::Document;
+pub(crate) use crate::error::{bail, DatashedError, DatashedResult};
+pub(crate) use crate::progress::ProgressBarBuilder;
